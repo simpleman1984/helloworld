a5f3b927cfd607fcd630e5e6388545717a3f145a
2 <2@q.c>
2016-11-28 00:42:36 +0800

    1.加入MQTT协议 2.支持离线消息（已测试） 3.结束当前示例，开始在实际项目中进行修改。

--- cordova_base/helloworld/platforms/android/src/io/cordova/hellocordova/MainActivity.java
+++ cordova_base/helloworld/platforms/android/src/io/cordova/hellocordova/MainActivity.java
@@ -19,8 +19,20 @@
 
 package io.cordova.hellocordova;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+
 import org.apache.cordova.*;
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttAsyncClient;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class MainActivity extends CordovaActivity
 {
@@ -37,5 +49,71 @@ public class MainActivity extends CordovaActivity
 
         // Set by <content src="index.html" /> in config.xml
         loadUrl(launchUrl);
+
+        populateConnectionList();
+    }
+
+    IMqttAsyncClient mqttClient = null;
+    private void populateConnectionList(){
+        Context context = this.getApplicationContext();
+        mqttClient = new MqttAndroidClient(context, "tcp://192.168.31.75:1883", "testConnect");
+        try{
+            //设置消息监听器（接收消息使用）
+            MqttReceiver mqttV3Receiver = new MqttReceiver();
+            mqttClient.setCallback(mqttV3Receiver);
+            //设置mqtt参数
+            MqttConnectOptions mqttConnectOptions = new MqttConnectOptions();
+            mqttConnectOptions.setCleanSession(false);
+            mqttClient.connect(mqttConnectOptions,context,new ActionListener());
+        }catch(MqttException exception){
+            exception.printStackTrace();
+        }
+    }
+
+    private class MqttReceiver implements MqttCallback
+    {
+
+        @Override
+        public void connectionLost(Throwable cause) {
+        }
+
+        @Override
+        public void messageArrived(String topic, MqttMessage message) throws Exception {
+            System.out.println("topic:  "+topic+"消息接收成功啦！！！！！！！！！" + new String(message.getPayload()));
+            mqttClient.publish("topic.confirmed",new String(new String(message.getPayload())+"__confirmed").getBytes(),0,false);
+        }
+
+        @Override
+        public void deliveryComplete(IMqttDeliveryToken token) {
+        }
+    }
+
+    private class ActionListener implements IMqttActionListener {
+        @Override
+        public void onSuccess(IMqttToken asyncActionToken) {
+            try{
+                String[] topicNames = new String[]{"/Android/Topic"};
+                //QoS level 0
+                //至多发送一次，发送即丢弃。没有确认消息，也不知道对方是否收到。
+                //QoS level 1
+                //Qos level 1，意味着消息至少被传输一次。
+                //QoS level 2
+                //但确保了仅仅传输接收一次
+                //当前cleanSession设置为false，且QOS为2，则默认为activemq的持久化消息（客户端掉线后，回来还能收到）！
+                int[] topicQos = {2};
+                mqttClient.subscribe(topicNames, topicQos);
+            }catch(MqttException e){
+                e.printStackTrace();
+            }
+            Log.d(TAG, "Success. Release lock(消息服务器连接成功！！！！！！！！！！！):"
+                    + System.currentTimeMillis());
+        }
+
+        @Override
+        public void onFailure(IMqttToken asyncActionToken,
+                              Throwable exception) {
+            Log.d(TAG, "Failure. Release lock(消息服务器连接失败！！！！！！！！！！！):"
+                    + System.currentTimeMillis());
+        }
     }
 }
