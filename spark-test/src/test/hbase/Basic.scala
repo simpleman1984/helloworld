f6a60920fa47841d37dcd79110b49743dc17d4de
s <ss>
2017-01-13 15:10:17 +0800

    1.加入了aes算法

--- spark-test/src/test/hbase/Basic.scala
+++ spark-test/src/test/hbase/Basic.scala
@@ -18,6 +18,12 @@ import org.apache.hadoop.hbase.filter.SingleColumnValueFilter
 import org.apache.hadoop.hbase.filter.CompareFilter.CompareOp
 import org.apache.hadoop.hbase.client.Scan
 import org.apache.hadoop.util.GenericOptionsParser
+import org.apache.hadoop.hbase.client.Connection
+import org.apache.spark.rdd.JdbcRDD.ConnectionFactory
+import org.apache.hadoop.hbase.client.ConnectionFactory
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+import org.apache.hadoop.hbase.client.Table
 
 object Basic {
 
@@ -28,6 +34,12 @@ object Basic {
 
     System.setProperty("hadoop.home.dir", "D:/workspace/git_lab/spark-test/hadoop");
 
+    //屏蔽不必要的日志显示在终端上
+    Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+    Logger.getLogger("org.apache.eclipse.jetty.server").setLevel(Level.OFF)
+    Logger.getLogger("org.spark_project.jetty.server").setLevel(Level.OFF)
+    Logger.getLogger("org.apache.zookeeper.ZooKeeper").setLevel(Level.WARN)
+
     val sparkConf = new SparkConf().setAppName("MovieLensALS").setMaster("local[5]")
     val sc = new SparkContext(sparkConf)
 
@@ -35,52 +47,53 @@ object Basic {
     config.addResource(new Path("/src/test/hbase/core-site.xml"));
 
     //hbase客户端示例
-    //    val admin = new HBaseAdmin(config)
+    val connection = ConnectionFactory.createConnection(config)
+    val admin = connection.getAdmin();
 
-    //    val tableName = "test"
+    //    val tableName = TableName.valueOf("test");
     //    val columnDesc = new HColumnDescriptor("cf1");
-    //    admin.disableTable(Bytes.toBytes(tableName));
-    //    admin.addColumn(tableName, columnDesc);
-    //    admin.enableTable(Bytes.toBytes(tableName));
+    //    admin.enableTable(tableName);
+    //    //    admin.disableTable(tableName);
+    //    admin.addColumnFamily(tableName, columnDesc);
 
-    //    val tables = admin.listTables();
-    //    tables.foreach(println)
+    val tables = admin.listTables();
+    tables.foreach(println)
 
-    // 以下代码示例为插入新的数据
-    // create the table...
-    //     val tableDescriptor = new HTableDescriptor(TableName.valueOf("people"));
-    //     // ... with two column families
-    //     tableDescriptor.addFamily(new HColumnDescriptor("name"));
-    //     tableDescriptor.addFamily(new HColumnDescriptor("contactinfo"));
-    //     admin.createTable(tableDescriptor);
-    //
-    //     // define some people
-    //     val peoples = List(
-    //         Array( "1", "Marcel", "Haddad", "marcel@fabrikam.com"),
-    //         Array( "2", "Franklin", "Holtz", "franklin@contoso.com" ),
-    //         Array( "3", "Dwayne", "McKee", "dwayne@fabrikam.com" ),
-    //         Array( "4", "Rae", "Schroeder", "rae@contoso.com" ),
-    //         Array( "5", "Rosalie", "burton", "rosalie@fabrikam.com"),
-    //         Array( "6", "Gabriela", "Ingram", "gabriela@contoso.com") );
-    //
-    //     val table = new HTable(config, "people");
+    //没有表people则创建
+    val tableName = TableName.valueOf("test1")
+    //创建表，加入默认的簇
+    val tableDescriptor = new HTableDescriptor(tableName);
+    tableDescriptor.addFamily(new HColumnDescriptor("name"));
+    tableDescriptor.addFamily(new HColumnDescriptor("contactinfo"));
+    //    admin.createTable(tableDescriptor);
+
+    //往表中插入数据
+    val peoples = List(
+      Array("1", "Marcel", "Haddad", "marcel@fabrikam.com"),
+      Array("2", "Franklin", "Holtz", "franklin@contoso.com"),
+      Array("3", "Dwayne", "McKee", "dwayne@fabrikam.com"),
+      Array("4", "Rae", "Schroeder", "rae@contoso.com"),
+      Array("5", "Rosalie", "burton", "rosalie@fabrikam.com"),
+      Array("6", "Gabriela", "Ingram", "gabriela@contoso.com"));
+
+    val table = connection.getTable(tableName);
 
     // Add each person to the table
     //   Use the `name` column family for the name
     //   Use the `contactinfo` column family for the email
-    //     peoples.foreach{(_people) =>
-    //       val person = new Put(Bytes.toBytes(_people(0)))
-    //       person.add(Bytes.toBytes("name"), Bytes.toBytes("first"), Bytes.toBytes(_people(1)))
-    //       person.add(Bytes.toBytes("name"), Bytes.toBytes("last"), Bytes.toBytes(_people(2)))
-    //       person.add(Bytes.toBytes("contactinfo"), Bytes.toBytes("email"), Bytes.toBytes(_people(3)))
-    //       table.put(person);
-    //     }
-    //     // flush commits and close the table
-    //     table.flushCommits();
-    //     table.close();
-    //     
-    // Use GenericOptionsParser to get only the parameters to the class
-    // and not all the parameters passed (when using WebHCat for example)
+    //向数据库中插入数据~
+    peoples.foreach { (_people) =>
+      val person = new Put(Bytes.toBytes(_people(0)))
+      person.addColumn(Bytes.toBytes("name"), Bytes.toBytes("first"), Bytes.toBytes(_people(1)))
+      person.addColumn(Bytes.toBytes("name"), Bytes.toBytes("last"), Bytes.toBytes(_people(2)))
+      person.addColumn(Bytes.toBytes("contactinfo"), Bytes.toBytes("email"), Bytes.toBytes(_people(3)))
+      table.put(person);
+    }
+    // flush commits and close the table
+    //    admin.flush(tableName)
+    table.close();
+
+    //解析命令行參數
     var _args = Array("fabrikam");
     val otherArgs = new GenericOptionsParser(config, _args).getRemainingArgs();
     if (otherArgs.length != 1) {
@@ -88,49 +101,48 @@ object Basic {
       System.exit(-1);
     }
 
-    // Open the table
-    //    val table = new HTable(config, "people");
-
-    // Define the family and qualifiers to be used
-    //    val contactFamily = Bytes.toBytes("contactinfo")
-    //    val emailQualifier = Bytes.toBytes("email")
-    //    val nameFamily = Bytes.toBytes("name")
-    //    val firstNameQualifier = Bytes.toBytes("first")
-    //    val lastNameQualifier = Bytes.toBytes("last")
+    //设置查询参数
+    val contactFamily = Bytes.toBytes("contactinfo")
+    val emailQualifier = Bytes.toBytes("email")
+    val nameFamily = Bytes.toBytes("name")
+    val firstNameQualifier = Bytes.toBytes("first")
+    val lastNameQualifier = Bytes.toBytes("last")
 
     // Create a new regex filter
-    //    val emailFilter = new RegexStringComparator(otherArgs(0));
+    val emailFilter = new RegexStringComparator(otherArgs(0));
     // Attach the regex filter to a filter
     //   for the email column
-    //    val filter = new SingleColumnValueFilter(
-    //      contactFamily,
-    //      emailQualifier,
-    //      CompareOp.EQUAL,
-    //      emailFilter);
+    val filter = new SingleColumnValueFilter(
+      contactFamily,
+      emailQualifier,
+      CompareOp.EQUAL,
+      emailFilter);
 
     // Create a scan and set the filter
-    //    val scan = new Scan();
-    //    scan.setFilter(filter);
+    val scan = new Scan();
+    scan.setFilter(filter);
 
     // Get the results
-    //    val results = table.getScanner(scan);
-    //    // Iterate over results and print  values
-    //    var it = results.iterator();
-    //    while (it.hasNext()) {
-    //      var result = results.next();
-    //      val id = new String(result.getRow());
-    //      val firstNameObj = result.getValue(nameFamily, firstNameQualifier);
-    //      val firstName = new String(firstNameObj);
-    //      val lastNameObj = result.getValue(nameFamily, lastNameQualifier);
-    //      val lastName = new String(lastNameObj);
-    //      System.out.println(firstName + " " + lastName + " - ID: " + id);
-    //      val emailObj = result.getValue(contactFamily, emailQualifier);
-    //      val email = new String(emailObj);
-    //      System.out.println(firstName + " " + lastName + " - " + email + " - ID: " + id);
-    //    }
-    //
-    //    results.close();
-    //    table.close();
+    val results = table.getScanner(scan);
+    // Iterate over results and print  values
+    var it = results.iterator();
+    while (it.hasNext()) {
+      var result = results.next();
+      if (result != null) {
+        val id = new String(result.getRow());
+        val firstNameObj = result.getValue(nameFamily, firstNameQualifier);
+        val firstName = new String(firstNameObj);
+        val lastNameObj = result.getValue(nameFamily, lastNameQualifier);
+        val lastName = new String(lastNameObj);
+        System.out.println("查询结果:     " + firstName + " " + lastName + " - ID: " + id);
+        val emailObj = result.getValue(contactFamily, emailQualifier);
+        val email = new String(emailObj);
+        System.out.println("查询结果:     " + firstName + " " + lastName + " - " + email + " - ID: " + id);
+      }
+    }
+
+    results.close();
+    table.close();
   }
 
 }
--- spark-test/src/test/hbase/SparkHBase.scala
+++ spark-test/src/test/hbase/SparkHBase.scala
@@ -10,9 +10,18 @@ import org.apache.hadoop.hbase.spark.HBaseContext
 import org.apache.hadoop.hbase.TableName
 import org.apache.hadoop.hbase.client.Put
 import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.hbase.CellUtil
+import org.apache.hadoop.hbase.client.Get
+import org.apache.hadoop.hbase.client.Result
 
 object SparkHBase {
   def main(args: Array[String]) {
+    //有比较详细的spark和hbase的例子
+    //http://blog.cloudera.com/blog/2015/08/apache-spark-comes-to-apache-hbase-with-hbase-spark-module/
+    //github上各种示例
+    //https://github.com/apache/hbase/blob/master/hbase-spark/src/main/java/org/apache/hadoop/hbase/spark/example/hbasecontext/JavaHBaseBulkGetExample.java
+    //https://github.com/tmalaska/SparkOnHBase/blob/master/src/main/scala/org/apache/hadoop/hbase/spark/example/hbasecontext/HBaseBulkGetExample.scala
+
     System.setProperty("hadoop.home.dir", "D:/workspace/git_lab/spark-test/hadoop");
     //屏蔽不必要的日志显示在终端上
     Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
@@ -27,8 +36,50 @@ object SparkHBase {
 
     val textFile = sc.textFile("src/test/hbase/hbase.dat")
 
-    val tableName = TableName.valueOf("tableName");
+    val tableName = TableName.valueOf("test1");
     val hbaseContext = new HBaseContext(sc, config)
+
+    val list = List(Bytes.toBytes("1"), Bytes.toBytes("2"));
+    val rdd = sc.parallelize(Array(
+      Bytes.toBytes("1"),
+      Bytes.toBytes("2"),
+      Bytes.toBytes("3"),
+      Bytes.toBytes("4"),
+      Bytes.toBytes("5"),
+      Bytes.toBytes("6"),
+      Bytes.toBytes("7")))
+
+    //Future Work
+    //未来Work数据
+
+    val getRdd = hbaseContext.bulkGet[Array[Byte], String](tableName,
+      2,
+      rdd,
+      record => {
+        System.out.println("making Get:" + record)
+        new Get(record)
+      },
+      (result: Result) => {
+
+        val it = result.listCells().iterator()
+        val b = new StringBuilder
+
+        b.append(Bytes.toString(result.getRow) + ":")
+
+        while (it.hasNext) {
+          val cell = it.next()
+          val q = Bytes.toString(CellUtil.cloneQualifier(cell))
+          if (q.equals("counter")) {
+            b.append("(" + q + "," + Bytes.toLong(CellUtil.cloneValue(cell)) + ")")
+          } else {
+            b.append("(" + q + "," + Bytes.toString(CellUtil.cloneValue(cell)) + ")")
+          }
+        }
+        b.toString()
+      })
+
+    getRdd.collect().foreach(v => println(v))
+
     hbaseContext.bulkPut[String](textFile, tableName, (putRecord) => {
       System.out.println("hbase-" + putRecord)
       val put = new Put(Bytes.toBytes("Value- " + putRecord))
@@ -36,5 +87,7 @@ object SparkHBase {
         Bytes.toBytes(putRecord.length()))
       put
     });
+
   }
+
 }
