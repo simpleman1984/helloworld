b84ffee457fa1b11abd75c4d5d59918022da7a96
simpleman1984 <simpleman1984@126.com>
2017-06-21 11:36:36 +0800

    1.加入vuejs示例。

---
+++ h5_vuejs_templates/webpack/src/components/Hello.vue
@@ -0,0 +1,17 @@
+<template src="./Hello.html">
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped src="./Hello.css">
+</style>
